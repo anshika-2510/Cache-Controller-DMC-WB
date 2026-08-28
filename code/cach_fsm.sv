6421c1f3baa14ed58cda24af67cc07a34bd391b3
Anshika Gupta <ag.anshika25@gmail.com>
2026-08-28 22:50:30 +0530

    Update and rename design.sv to cach_fsm.sv

--- code/cach_fsm.sv
+++ code/cach_fsm.sv
@@ -1,25 +1,25 @@
 `timescale 1ns / 1ps
-module cache2#(
+module cache_cdc #(
     parameter ADDR_W = 8,
     parameter DATA_W = 8,
     parameter NL  = 4,
     parameter WL = 1
-)( // CPU interface
-    input logic clk,
-    input  logic rst,                                                  
+)(
+    input logic clk_cpu,
+    input  logic rst_cpu,                                                  // CPU interface
     input logic cpu_valid,
     input  logic cpu_wr,
     input  logic [ADDR_W-1:0] cpu_addr,
     input  logic [DATA_W-1:0] cpu_wdata,
     output logic [DATA_W-1:0] cpu_rdata,
     output logic cpu_ready,
-    // Memory interface
-    output logic mem_valid,
-    output logic  mem_wr,
-    output logic [ADDR_W-1:0] mem_addr,
-    output logic [DATA_W-1:0] mem_wdata,
-    input  logic [DATA_W-1:0] mem_rdata,
-    input  logic  mem_ready
+    
+    output logic start,
+    input logic done, 
+    input logic [DATA_W-1:0] rdata_out,
+    output logic [ADDR_W-1:0] miss_addr,
+   output logic miss_wr,
+   output logic [DATA_W-1:0] miss_wdata
 );
     // Address widths
     localparam INDEX_W = $clog2(NL);
@@ -46,25 +46,38 @@ typedef enum logic [2:0] {
     MEM_READ=3'b001,
     REFILL=3'b010,
     RESPOND=3'b011,
-    WRITE_BACK=3'b100
+    WRITE_BACK=3'b100,
+    MEM_START =3'b101,
+    WRITE_BACK_START=3'b110
 } state_t;
 state_t state;
 
-logic[ADDR_W-1:0] miss_addr;
+
 logic [INDEX_W-1:0] miss_index;
 logic [TAG_W-1:0]   miss_tag;
-logic [DATA_W-1:0] miss_wdata;
-logic miss_wr;
+
 assign miss_index = miss_addr[INDEX_W-1:0];
 assign miss_tag = miss_addr[ADDR_W-1 : INDEX_W + OFFSET_W];
 
-always_ff @(posedge clk) begin
-    if (rst)
+logic[ADDR_W-1:0] org_addr;
+logic [INDEX_W-1:0] org_index;
+logic [TAG_W-1:0]   org_tag;
+logic [DATA_W-1:0] org_wdata;
+logic org_wr;
+assign org_index = org_addr[INDEX_W-1:0];
+assign org_tag = org_addr[ADDR_W-1 : INDEX_W + OFFSET_W];
+
+
+always_ff @(posedge clk_cpu) begin
+    if (rst_cpu)
      begin
         state     <= IDLE;
-        miss_addr <= '0;
+        miss_addr <= 0;
         cpu_ready <= 1'b0;
-        mem_valid <= 1'b0;
+        start <=1'b0;
+        miss_wr<=1'b0;
+        org_addr <=1'b0;
+        org_wr<=1'b0;
         for (i = 0; i < NL; i = i + 1) begin
             valid_array[i] <= 1'b0;
             dirty_array[i] <= 1'b0;
@@ -74,8 +87,7 @@ always_ff @(posedge clk) begin
     begin
         
         cpu_ready <= 1'b0;
-        mem_valid <= 1'b0;
-        mem_wr<=1'b0;
+        
 
         case (state)
             IDLE:
@@ -99,58 +111,70 @@ always_ff @(posedge clk) begin
                     else 
                     begin
                        
-                        miss_wr<=cpu_wr;
-                        miss_wdata<=cpu_wdata;
-                        miss_addr<=cpu_addr;
+                        org_wr<=cpu_wr;
+                        org_wdata<=cpu_wdata;
+                        org_addr<=cpu_addr;
                         if(valid_array[index] && dirty_array[index])
-                        state<= WRITE_BACK;
+                        state<= WRITE_BACK_START;
                         else
-                        state<=MEM_READ;
+                        state<=MEM_START;
                     end
                 end
             end
-
+          MEM_START:
+          begin
+          start<=1'b1;
+          state<=MEM_READ;
+         
+          end
             MEM_READ:
              begin
-                mem_valid <= 1'b1;
-                mem_addr  <= miss_addr;
-                if (mem_ready)
-                    state <= REFILL;
+             start<=0;
+               miss_wr<=org_wr;
+                miss_wdata<=org_wdata;
+                miss_addr<=org_addr;
+                if(done)
+                 state <= REFILL;
+                end
+           
+            WRITE_BACK_START:
+            begin
+            start<=1'b1;
+            state<=WRITE_BACK;
             end
-
-            REFILL:
+            WRITE_BACK:
              begin
-                tag_array[miss_index]   <= miss_tag;
-                valid_array[miss_index] <= 1'b1;
-                if(miss_wr)
-                begin
-                dirty_array[miss_index] <= 1'b1;
-                data_array[miss_index]  <= miss_wdata;
+             start<=0;
+               miss_wr<=1'b1;
+               miss_wdata<=data_array[org_index];
+               miss_addr<={tag_array[org_index], org_index[INDEX_W-1:0]};
+               if(done)
+               state<=MEM_START;//memread
+               
+           end
+                REFILL:
+                 begin
+                    tag_array[org_index]   <= org_tag;
+                    valid_array[org_index] <= 1'b1;
+                    if(org_wr)
+                    begin
+                    dirty_array[org_index] <= 1'b1;
+                    data_array[org_index]  <= org_wdata;
+                    end
+                    else
+                    begin
+                    dirty_array[org_index] <= 1'b0;
+                    data_array[org_index]  <= rdata_out;
+                    end
+                    state <= RESPOND;
                 end
-                else
-                begin
-                dirty_array[miss_index] <= 1'b0;
-                data_array[miss_index]  <= mem_rdata;
-                end
-                state <= RESPOND;
-            end
 
             RESPOND:
              begin
-                cpu_rdata <= data_array[miss_index];
+                cpu_rdata <= data_array[org_index];
                 cpu_ready <= 1'b1;
                 state     <= IDLE;
             end
-            
-            WRITE_BACK:
-             begin
-           mem_valid <= 1'b1;
-           mem_wr <= 1'b1;
-           mem_addr  <= {tag_array[miss_index], miss_index[INDEX_W-1:0]}; // old address being evicted
-           mem_wdata <= data_array[miss_index];
-          if (mem_ready)
-          state <= MEM_READ;
-           end
         endcase
     end
 end
